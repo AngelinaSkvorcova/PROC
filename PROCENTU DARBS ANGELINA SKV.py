908f580cd3df2386d1596f174121b238063e94d9
AngelinaSkvorcova <angelinaskvorcova72100@gmail.com>
2024-11-09 09:48:02 +0200

    Add files via upload

---
+++ PROCENTU DARBS ANGELINA SKV.py
@@ -0,0 +1,31 @@
+#1 uzd
+
+(sin(x)+1)/y+y/(x+1,5)
+
+#2uzd
+
+x=int(input(“x:”))
+y=int(input(“y:”))
+result1=(2+x)/(x*y)
+print(result1)
+
+#3 uzd
+
+x=int(input(“x:”))
+y=int(input(“y:”))
+result2=(5**(3-x)**2+7))/(x*(x-6))
+
+#4 uzd
+
+x=int(input(“x:”))
+y=int(input(“y:”))
+result3=
+print(result3)
+
+#5 uzd.
+
+x=int(input(”y:”))
+y=int(input(“x:”))
+result4=(2**(x*y))/(5*y)
+Print(result4)
+
