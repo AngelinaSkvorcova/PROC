f881dc5525a0b52049322b111fbbb22f9fdc3b50
GlebVasiljev <glebsvasiljevs@gmail.com>
2024-11-11 09:23:53 +0200

    Update PROCENTU DARBS ANGELINA SKV.py

--- PROCENTU DARBS ANGELINA SKV.py
+++ PROCENTU DARBS ANGELINA SKV.py
@@ -1,31 +1,28 @@
 #1 uzd
 
-(sin(x)+1)/y+y/(x+1,5)
+#(sin(x)+1)/y+y/(x+1,5) # nesaprotu kam jāizmanto sin() funkciju
 
 #2uzd
-
-x=int(input(“x:”))
-y=int(input(“y:”))
+x = int(input('x:')) # ” <- iespējams ka kāds Interpretators var šo zīme saprast, bet Spyder un VSC izmeta kļudu, līdz ar to iesaku izmantot parastas " " vai ' '
+y = int(input('y:'))
 result1=(2+x)/(x*y)
 print(result1)
 
 #3 uzd
-
-x=int(input(“x:”))
-y=int(input(“y:”))
-result2=(5**(3-x)**2+7))/(x*(x-6))
+x = int(input('x:'))
+y = int(input('y:'))
+result2=(5**(3-x)**2+7)/(x*(x-6))
+print(result2)
 
 #4 uzd
-
-x=int(input(“x:”))
-y=int(input(“y:”))
-result3=
+x = int(input('x:'))
+y = int(input('y:'))
+result3= ( x * y)**0.5 # Šeit ir piemērs kā var aizvietot kvadrātsakne https://wikimedia.org/api/rest_v1/media/math/render/svg/ab1002253803cf480d58170a99cedf4732590b1f
 print(result3)
 
 #5 uzd.
-
-x=int(input(”y:”))
-y=int(input(“x:”))
+x = int(input('x:'))
+y = int(input('y:'))
 result4=(2**(x*y))/(5*y)
-Print(result4)
+print(result4) # print obligāti jāraksta ar maziem burtiem 
 
